0316828252edefd762320fe6b19d790b6e8ca124
Nahom Tamene <tuo99862@temple.edu>
2025-04-02 15:28:43 -0400

    Implement coroutine

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -7,6 +7,9 @@ import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
@@ -21,24 +24,20 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.currentTextView)
     }
 
-    val handler = Handler(Looper.getMainLooper(), Handler.Callback {
-
-        currentTextView.text = String.format(Locale.getDefault(), "Current opacity: %d", it.what)
-        cakeImageView.alpha = it.what / 100f
-        true
-    })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
+        findViewById<Button>(R.id.revealButton).setOnClickListener {
+            lifecycleScope.launch {
+                for (i in 0..99) {
+                    currentTextView.text =
+                        String.format(Locale.getDefault(), "Current opacity: %d", i)
+                    cakeImageView.alpha = i / 100f
+                    delay(40)
                 }
-            }.start()
+            }
         }
     }
 }
